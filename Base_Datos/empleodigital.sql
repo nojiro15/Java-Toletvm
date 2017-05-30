17f4fd0fda1ff42a4162306be37b2d52de100896
Mario <mario_uce_gar@outlook.com>
2017-05-30 13:18:48 +0200

    Subida de la base de datos

---
+++ Base_Datos/empleodigital.sql
@@ -0,0 +1,295 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-05-2017 a las 14:22:48
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empleodigital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fiesta_local`
+--
+
+CREATE TABLE `fiesta_local` (
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `localidad` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fiesta_nacional`
+--
+
+CREATE TABLE `fiesta_nacional` (
+  `fecha` date NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fiesta_regional`
+--
+
+CREATE TABLE `fiesta_regional` (
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `region` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formaciones`
+--
+
+CREATE TABLE `formaciones` (
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formaciones_localidades`
+--
+
+CREATE TABLE `formaciones_localidades` (
+  `id_formacion` int(11) NOT NULL,
+  `nombre_localidad` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formaciones_modulos`
+--
+
+CREATE TABLE `formaciones_modulos` (
+  `id_formacion` int(11) NOT NULL,
+  `id_modulo` int(11) NOT NULL,
+  `orden` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidades`
+--
+
+CREATE TABLE `localidades` (
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `region` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulos`
+--
+
+CREATE TABLE `modulos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `jornadas` int(10) NOT NULL,
+  `horas` int(10) NOT NULL,
+  `horas_tutorias` int(10) NOT NULL,
+  `bloque` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `regiones`
+--
+
+CREATE TABLE `regiones` (
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `enabled` smallint(6) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacaciones`
+--
+
+CREATE TABLE `vacaciones` (
+  `id` int(11) NOT NULL,
+  `asunto` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `formacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `fiesta_local`
+--
+ALTER TABLE `fiesta_local`
+  ADD PRIMARY KEY (`fecha`),
+  ADD KEY `region` (`localidad`);
+
+--
+-- Indices de la tabla `fiesta_nacional`
+--
+ALTER TABLE `fiesta_nacional`
+  ADD PRIMARY KEY (`fecha`);
+
+--
+-- Indices de la tabla `fiesta_regional`
+--
+ALTER TABLE `fiesta_regional`
+  ADD PRIMARY KEY (`fecha`),
+  ADD KEY `region` (`region`);
+
+--
+-- Indices de la tabla `formaciones`
+--
+ALTER TABLE `formaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `formaciones_localidades`
+--
+ALTER TABLE `formaciones_localidades`
+  ADD PRIMARY KEY (`id_formacion`,`nombre_localidad`),
+  ADD KEY `nombre_localidad` (`nombre_localidad`);
+
+--
+-- Indices de la tabla `formaciones_modulos`
+--
+ALTER TABLE `formaciones_modulos`
+  ADD PRIMARY KEY (`id_formacion`,`id_modulo`),
+  ADD KEY `id_modulo` (`id_modulo`);
+
+--
+-- Indices de la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  ADD PRIMARY KEY (`nombre`),
+  ADD KEY `region` (`region`);
+
+--
+-- Indices de la tabla `modulos`
+--
+ALTER TABLE `modulos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `regiones`
+--
+ALTER TABLE `regiones`
+  ADD PRIMARY KEY (`nombre`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indices de la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `formacion` (`formacion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `formaciones`
+--
+ALTER TABLE `formaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `modulos`
+--
+ALTER TABLE `modulos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `fiesta_local`
+--
+ALTER TABLE `fiesta_local`
+  ADD CONSTRAINT `fiesta_local_ibfk_1` FOREIGN KEY (`localidad`) REFERENCES `localidades` (`nombre`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `fiesta_regional`
+--
+ALTER TABLE `fiesta_regional`
+  ADD CONSTRAINT `fiesta_regional_ibfk_1` FOREIGN KEY (`region`) REFERENCES `regiones` (`nombre`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `formaciones_localidades`
+--
+ALTER TABLE `formaciones_localidades`
+  ADD CONSTRAINT `formaciones_localidades_ibfk_1` FOREIGN KEY (`id_formacion`) REFERENCES `formaciones` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `formaciones_localidades_ibfk_2` FOREIGN KEY (`nombre_localidad`) REFERENCES `localidades` (`nombre`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `formaciones_modulos`
+--
+ALTER TABLE `formaciones_modulos`
+  ADD CONSTRAINT `formaciones_modulos_ibfk_1` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `formaciones_modulos_ibfk_2` FOREIGN KEY (`id_formacion`) REFERENCES `formaciones` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  ADD CONSTRAINT `localidades_ibfk_1` FOREIGN KEY (`region`) REFERENCES `regiones` (`nombre`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD CONSTRAINT `vacaciones_ibfk_1` FOREIGN KEY (`formacion`) REFERENCES `formaciones` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
