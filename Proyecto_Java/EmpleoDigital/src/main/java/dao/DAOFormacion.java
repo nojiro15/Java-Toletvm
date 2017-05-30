0e8fe7d0688352c5fe0ea51ff982ff67b68136b9
Mario <mario_uce_gar@outlook.com>
2017-05-30 13:19:46 +0200

    Subida del proyecto creado

---
+++ Proyecto_Java/EmpleoDigital/src/main/java/dao/DAOFormacion.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.List;
+
+import modelos.Formacion;
+
+
+public interface DAOFormacion {
+	
+	public boolean create(Formacion f);
+	public Formacion read(String palabra);
+	public boolean update(Formacion f);
+	public boolean delete(Formacion f);
+	public List<Formacion> listar();
+	
+}
---
+++ Proyecto_Java/EmpleoDigital/src/main/java/dao/DAOModulo.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+import modelos.Modulo;
+
+public interface DAOModulo {
+	
+	public boolean create(Modulo m);
+	public Modulo read(String palabra);
+	public boolean update(Modulo m);
+	public boolean delete(Modulo m);
+	public List<Modulo> listar();
+	
+}
